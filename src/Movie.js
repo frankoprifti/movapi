d8cafeb754def296a05f8e068871f2a529293b49
Franko Prifti <frankoprifti@gmail.com>
2020-05-18 20:46:10 +0200

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -10,7 +10,7 @@ export default class Movie extends Component {
       <div>
         <div className="App">
           <iframe
-            style={{position:absolute; width: "100%", height: "100%", borderWidth: 0 }}
+            style={{position:"absolute", width: "100%", height: "100%", borderWidth: 0 }}
             src={this.url}
             sandbox="allow-forms allow-pointer-locks allow-same-origin allow-scripts"
             allowFullScreen
