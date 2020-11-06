299856d593d67e31c58ef13f02405ff3b3869d46
Franko Prifti <frankoprifti@gmail.com>
2020-11-06 13:51:35 +0100

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import * as qs from "query-string";
+
 export default class Movie extends Component {
   id = window.location.search.split("=")[1];
 
@@ -10,6 +10,7 @@ export default class Movie extends Component {
       <div>
         <div className="App">
           <iframe
+title="movie"
             style={{ width: "100%", height: "100vh", borderWidth: 0 }}
             src={this.url}
             sandbox="allow-forms allow-pointer-locks allow-same-origin allow-scripts"
